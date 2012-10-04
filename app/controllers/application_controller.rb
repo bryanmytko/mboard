dd89919713b9a3debb3c262a8e7d7e0ee9028b4b
Bryan Mytko <bryanmytko@gmail.com>
2012-10-04 12:46:34 -0400

    Fixed gravatar issue. Created user/author methods.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   private
   
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    @current_user ||= User.find( session[:user_id] ) if session[:user_id]
   end
   helper_method :current_user
   
@@ -13,9 +13,15 @@ class ApplicationController < ActionController::Base
     redirect_to login_url if current_user.nil?
   end
   
-  def gravatar_request
-    @user = User.find_by_id(session[:user_id])
-    @gravatar = Digest::MD5.hexdigest(@user.email)
+  def gravatar_self
+    user = User.find_by_id( session[:user_id] )
+    gravatar = Digest::MD5.hexdigest( user.email )
   end
-  helper_method :gravatar_request
+  helper_method :gravatar_self
+  
+  def gravatar_user( author )
+    user = User.find_by_username( author )
+    gravatar = Digest::MD5.hexdigest( user.email )
+  end
+  helper_method :gravatar_user
 end

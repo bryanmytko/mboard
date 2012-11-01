68ff66f2c24eeb0ce1bd17afdcd6459982816b4c
Bryan Mytko <bryanmytko@gmail.com>
2012-11-01 11:43:17 -0400

    added last/first to pagination. topic links now refer you to the last page. added last page method in home helper

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -2,4 +2,8 @@ module HomeHelper
   def reply_count(id)
     Reply.find_all_by_topic_id(id).count
   end
+  
+  def last_page id
+    reply_count( id ) / ENV['reply_pagination_count'].to_i + 1
+  end
 end
